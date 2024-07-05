f9b6a4e0fa86cfb83aa4e8e168d969598be624e3
yunchocopie <yunnn687@naver.com>
2024-07-05 11:10:58 +0900

    header bootstrap 완성

--- src/main/webapp/frontapp/src/App.js
+++ src/main/webapp/frontapp/src/App.js
@@ -1,29 +1,9 @@
-import { Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <Navbar bg="dark" expand="lg" variant="dark">
-        <Link to="/" className="navbar-brand">
-          블로그홈
-        </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Link to="/saveForm" className="nav-link">
-              글쓰기
-            </Link>
-            <Link className="nav-link">로그아웃</Link>
-            <Link to="/loginForm" className="nav-link">
-              로그인
-            </Link>
-            <Link to="/joinForm" className="nav-link">
-              회원가입
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Header />
     </div>
   );
 }
